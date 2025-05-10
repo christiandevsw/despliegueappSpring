bf1329faa421da27543f4c3c88d0de0a00589ec0
Christian Ramirez <christian.devsw@gmai.com>
2025-05-10 16:52:42 -0500

    primer commit

---
+++ src/test/java/org/example/exampledespliegue/ExampledespliegueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exampledespliegue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampledespliegueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
